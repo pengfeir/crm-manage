8419181739fa02b75538ed0fc8dad0b56b7a0ac2
乔登涛 <qiaodengtao@everjiankang.com>
2019-03-23 22:04:02 +0800

    反代理配置

--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,20 @@
  * @Author: renpengfei
  * @Date: 2019-03-16 18:46:59
  * @Last Modified by: renpengfei
- * @Last Modified time: 2019-03-18 15:20:52
+ * @Last Modified time: 2019-03-23 18:20:41
  */
 module.exports = {
   devServer: {
     // proxy: {
-    //   '/api': {
+    //   '/manage': {
     //     target: 'http://47.104.232.127:8080/', // 对应自己的接口
     //     changeOrigin: true,
     //     ws: true,
     //     pathRewrite: {
-    //       '^/api': ''
+    //       '^/manage': '/'
     //     }
     //   }
     // }
-    proxy: 'http://47.104.232.127:8080/'
+    proxy: 'http://47.104.232.127'
   }
 }
