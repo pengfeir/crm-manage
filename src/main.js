8b30326624db0e6929dcbdbd9d47d3f58b957f0b
乔登涛 <qiaodengtao@everjiankang.com>
2019-03-25 10:34:50 +0800

    新增编辑页头部

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import Element from 'element-ui'
 import router from './router'
 import { MyPlugin } from './plugins/common'
 import everPagination from './plugins/pagination'
+import everBreadCrumb from './plugins/everbreadcrumb'
 import 'element-ui/lib/theme-chalk/index.css'
 require('./plugins/funcform')
 Vue.use(MyPlugin)
 Vue.use(Element)
 Vue.component('everPagination', everPagination)
-
+Vue.component('everBreadCrumb', everBreadCrumb)
 Vue.config.productionTip = false
 new Vue({
   router,
