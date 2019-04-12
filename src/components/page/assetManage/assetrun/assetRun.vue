d10fd7020fbeed73bba24a09719afffa61c717e3
乔登涛 <qiaodengtao@everjiankang.com>
2019-04-12 19:23:24 +0800

    ui

--- src/components/page/assetManage/assetrun/assetRun.vue
+++ src/components/page/assetManage/assetrun/assetRun.vue
@@ -12,42 +12,49 @@
     <el-table v-loading="loading" :data="tableData" style="width: 100%" border stripe>
         <el-table-column type="index" width="50" label="序号">
         </el-table-column>
-        <el-table-column prop="assetStatus" label="设备状态">
+        <el-table-column prop="temperature" label="设备名称" width="100">
+        </el-table-column>
+        <el-table-column prop="temperature" label="设备型号" width="100">
+        </el-table-column>
+        <el-table-column prop="temperature" label="设备SN序列号" width="120">
+        </el-table-column>
+        <el-table-column prop="assetStatus" label="设备状态" width="100">
             <template slot-scope="scope">
                 {{scope.row.assetStatus | filterAssetStatus}}
             </template>
         </el-table-column>
-        <el-table-column prop="temperature" label="温度">
+        <el-table-column prop="temperature" label="温度" width="50">
         </el-table-column>
-        <el-table-column prop="energy" label="电能计量">
+        <el-table-column prop="energy" label="电能计量" width="80">
         </el-table-column>
-        <el-table-column prop="inputI" label="输入电流">
+        <el-table-column prop="inputI" label="输入电流" width="80">
         </el-table-column>
-        <el-table-column prop="inputV" label="输入电压">
+        <el-table-column prop="inputV" label="输入电压" width="80">
         </el-table-column>
         <el-table-column prop="macAddr" width="130" label="设备的MAC地址">
         </el-table-column>
-        <!-- <el-table-column prop="pos1" label="位置信息1">
+        <el-table-column prop="pos1" label="位置信息1" width="130">
         </el-table-column>
-        <el-table-column prop="pos2" label="位置信息2">
+        <el-table-column prop="pos2" label="位置信息2" width="130">
         </el-table-column>
-        <el-table-column prop="pos3" label="位置信息3">
-        </el-table-column> -->
-        <el-table-column prop="powerFactor" label="功率因数">
+        <el-table-column prop="pos3" label="位置信息3" width="130">
         </el-table-column>
-        <el-table-column prop="powerHz" label="电源频率">
+        <el-table-column prop="powerFactor" label="功率因数" width="80">
         </el-table-column>
-        <el-table-column prop="realPower" label="有功功率">
+        <el-table-column prop="powerHz" label="电源频率" width="80">
         </el-table-column>
-        <!-- <el-table-column prop="mtime" label="更新时间" width="180">
+        <el-table-column prop="realPower" label="有功功率" width="80">
+        </el-table-column>
+        <el-table-column prop="mtime" label="更新时间" width="180">
         </el-table-column>
         <el-table-column prop="ctime" label="创建时间"  width="180">
         </el-table-column>
         <el-table-column prop="extra" label="其他扩展信息" width="150">
         </el-table-column>
+        <!-- 
         <el-table-column prop="userId" label="创建者ID" width="180">
         </el-table-column> -->
-        <el-table-column prop="name" align="center" label="操作" width="100">
+        <el-table-column prop="name" align="center" label="操作" fixed="right" width="100">
           <template slot-scope="scope">
             <el-button type="text" icon="el-icon-search" @click="seeDetails(scope.row)">查看</el-button>
           </template>
@@ -123,11 +130,7 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.scroll {
-  height: 400px;
-  overflow: hidden;
-  overflow-y: scroll;
-}
+
 </style>
 
 
